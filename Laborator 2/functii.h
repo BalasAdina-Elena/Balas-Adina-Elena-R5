bf9f9462a32a1b1b59aecaf7415c566a3a1eb71d
adinaa_b <balasadina127@gmail.com>
2026-03-03 15:18:15 +0200

    Delete Laborator 2 directory

--- Laborator 2/functii.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "student.h"
-
-int cmp_nume( student& s1, student& s2);
-int cmp_matematica( student s1,  student s2);
-int cmp_engleza( student s1,  student s2);
-int cmp_istorie( student s1,  student s2);
-int cmp_medie( student s1,  student s2);
